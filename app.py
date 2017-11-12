06e89d2ecb55545438b6c9a6a437421e4b61cd92
Hyhyme <eluo1@stuy.edu>
2017-11-12 09:58:37 -0500

    added Newton functionality

--- app.py
+++ app.py
@@ -6,10 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=OUxIy0hwIPwpys8dzkVjUXJW5SOVYcsN6ofZkyvY")
-    data = u.read()
-    j = json.loads(data)
-    return render_template("nasa.html", img = j["hdurl"], title = j["title"], date = j["date"], explanation = j["explanation"], rights = j["copyright"])
+    
+    nasa = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=OUxIy0hwIPwpys8dzkVjUXJW5SOVYcsN6ofZkyvY")
+    picOfDay = nasa.read()
+    cool = json.loads(picOfDay)
+
+    #change these to calculate something else! Note the url-encoded " ^ "
+    op = "factor"
+    ex = "x%5E2 - 1"
+    
+    newton = urllib2.urlopen("https://newton.now.sh/" + op + "/" + ex)
+    calc = newton.read()
+    j = json.loads(calc)
+
+    return render_template("thing.html", img = cool["hdurl"], title = cool["title"], date = cool["date"], explanation = cool["explanation"], rights = cool["copyright"], operation = j["operation"], expression = j["expression"], result = j["result"])
 
 if __name__ == "__main__":
     app.debug = True
